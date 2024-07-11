9dca5e27c8e14b3cf8f9fde18e8f6a8c7eee68e4
Alec Singer <51802911+Sosenteam@users.noreply.github.com>
2024-07-11 08:47:58 -0700

    Got Circles to stay in bounds (Version 4)

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
-// welcome to blot!
+/*
+@title: InterlappingCircles
+@author: sosenteam
+@snapshot: interlappingCirclePhoto.png
+*/
+//Changeable Parameters:
 
-// check out this guide to learn how to program in blot
-// https://blot.hackclub.com/editor?guide=start
 
+let pointCount = 5; //Number of Points
+let minRingDist = 1.0; //Minium Distance between circles
+let maxRingSize = 50; // Maxium size of circle
+let circleResolution = 150; // Amount of points per circle
+
+//Define Box
 const width = 125;
 const height = 125;
-
 setDocDimensions(width, height);
+//Define Edges for cut opperation
+let bounds = [
+  [0, 0],
+  [width, 0],
+  [width, height],
+  [0, height]
+];
 
-// store final lines here
-const finalLines = [];
+
+// final lines
+let finalLines = [];
 
 // create a list of points
 let pointList = [];
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < pointCount; i++) {
   pointList.push([randomPoint()]);
 }
+
+//for each point, draw multiple circles around the point using the unit circle (woo trig!)
 for (let pointCount = 0; pointCount < pointList.length; pointCount++) {
   let point = pointList[pointCount][0]
-  for (let dist = bt.randIntInRange(0,10); dist < 50; dist += bt.rand()*10) {
-    let circle = [];
 
-    for (let a = 0; a < 6.28; a += 6.28 / 200) {
-      let x = point[0] + (Math.cos(a) * dist);
-      let y = point[1] + (Math.sin(a) * dist);
+  for (let dist = bt.randIntInRange(0, 10); dist < maxRingSize; dist += (bt.rand() + minRingDist) * 10) {
+    let circle = [];
+    //angle goes to 6.28 (2pi radians). 200 is the amount of points in the circle (resolution of circle)
+    for (let angle = 0; angle < 6.28; angle += 6.28 / circleResolution) {
+      let x = point[0] + (Math.cos(angle) * (dist));
+      let y = point[1] + (Math.sin(angle) * (dist));
       circle.push([x, y]);
     }
-        finalLines.push(bt.cut())
-    //finalLines.push(circle);
+    //Add each circle to the final draw list
+    finalLines.push(circle);
   }
 }
 
 
-// draw it
+// keep within edges
+finalLines = bt.cut(finalLines, [
+  [
+    [0, 0],
+    [width, 0],
+    [width, height],
+    [0, height]
+  ]
+]);
+//draw lines
 drawLines(finalLines);
 
-
+//Generate random point in graph
 function randomPoint() {
   let x = (bt.randInRange(1, width));
   let y = (bt.randInRange(1, height));
