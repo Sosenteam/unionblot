9ea35a948f31222187d6d950587c02930c03e393
Alec Singer <51802911+Sosenteam@users.noreply.github.com>
2024-07-11 08:47:31 -0700

    Version 3

--- index.js
+++ index.js
@@ -12,33 +12,25 @@ setDocDimensions(width, height);
 const finalLines = [];
 
 // create a list of points
-let pointList = [
-  [
-    [50, 50]
-  ],
-];
-for(let i = 0; i < 5; i++){
+let pointList = [];
+for (let i = 0; i < 5; i++) {
   pointList.push([randomPoint()]);
 }
-for(let pointCount = 0; pointCount<pointList.length;pointCount++){
-let point = pointList[pointCount][0]
-for (let dist = 10; dist < 50; dist += 10) {
-  let circle = [];
+for (let pointCount = 0; pointCount < pointList.length; pointCount++) {
+  let point = pointList[pointCount][0]
+  for (let dist = bt.randIntInRange(0,10); dist < 50; dist += bt.rand()*10) {
+    let circle = [];
 
-  for (let a = 0; a < 6.28; a += 6.28 / 200) {
-    let x = point[0] + (Math.cos(a) * dist);
-    let y = point[1] + (Math.sin(a) * dist);
-    circle.push([x, y]);
+    for (let a = 0; a < 6.28; a += 6.28 / 200) {
+      let x = point[0] + (Math.cos(a) * dist);
+      let y = point[1] + (Math.sin(a) * dist);
+      circle.push([x, y]);
+    }
+        finalLines.push(bt.cut())
+    //finalLines.push(circle);
   }
-
-  finalLines.push(circle);
-}
 }
 
-// add the polyline to the final lines
-// finalLines.push(circleList);
-
-// transform lines using the toolkit
 
 // draw it
 drawLines(finalLines);
